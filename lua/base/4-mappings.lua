9fbe0b34e393665dcb2a0f2311c298d8ea30e970
Jon Gao <jonatgao@amazon.com>
2024-09-25 05:33:09 +0000

    adding quick buffer switch with <C-q>

--- lua/base/4-mappings.lua
+++ lua/base/4-mappings.lua
@@ -860,6 +860,10 @@ if is_available("telescope.nvim") then
     end,
     desc = "Find nvim config files",
   }
+  maps.n["<C-q>"] = {
+    function() require("telescope.builtin").buffers({ ignore_current_buffer = true }) end,
+    desc = "Find buffers",
+  }
   maps.n["<leader>fB"] = {
     function() require("telescope.builtin").buffers() end,
     desc = "Find buffers",
