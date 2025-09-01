079dcbf033373c37a75a6d052d76bc15e577adea
Zeioth <zeioth@hotmail.com>
2025-09-01 15:52:55 +0200

    refactor(`set_url_effect`): Renamed to `set_url_hl`, which is the standard neovim way to refert to highlights.

--- lua/base/1-options.lua
+++ lua/base/1-options.lua
@@ -82,6 +82,5 @@ vim.g.icons_enabled = true -- Enable icons in the UI (disable if no nerd font is
 vim.g.lsp_round_borders_enabled = true -- Enable round borders for lsp hover and signatureHelp.
 vim.g.lsp_signature_enabled = true -- Enable automatically showing lsp help as you write function parameters.
 vim.g.notifications_enabled = true -- Enable notifications.
-vim.g.url_effect_enabled = true -- Highlight URLs with an underline effect.
-vim.g.minianimate_disable = true
+vim.g.url_hl_enabled = true -- Highlight URLs with an underline effect.
 
